c0f051171fae41ebe2aec1e6d449566618142ade
Michelle Yenny <mmy491@gmail.com>
2018-12-22 23:24:03 -0500

    Added axios to react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Welcome to the Safari!</h1>
+        <p>Here's a list of all the animals seen today!</p>
+        <p>Here are all the animals seen in the jungle today!</p>
+        <p>Delete all the animals in the desert.</p>
+        <p>The total number of animals seen today is:</p>
+        <p>
+          The total number of times Lions, Tigers, and Bears have been seen
+          today is:
+        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
