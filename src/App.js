2a2930ebc9b85da6fe440a79d343274b8d50e694
mmyenny <mmy491@gmail.com>
2019-01-05 00:35:32 -0500

    Fetched response data. Itterated over response data to display info

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+
 import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allAnimals: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:4567/Animals').then(response => {
+      this.setState({
+        allAnimals: response.data
+      })
+      console.log(this.state.allAnimals)
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Welcome to the Safari!</h1>
         <p>Here's a list of all the animals seen today!</p>
+        {this.state.allAnimals.map((animal, index) => {
+          // console.log(animal.species)
+          return (
+            <p className="animalList" key={index}>
+              {animal.species}
+            </p>
+          )
+        })}
         <p>Here are all the animals seen in the jungle today!</p>
         <p>Delete all the animals in the desert.</p>
         <p>The total number of animals seen today is:</p>
